39cdc369cb70338a0f8def9fda0fb5a7517ab1bf
lrscime <lrscime.alpha@gmail.com>
2019-10-03 21:22:34 -0400

    Finished point selection

--- Main.py
+++ Main.py
@@ -12,6 +12,7 @@ from pylab import ginput # get user mouse inputs
 import tkinter as tk # file UI
 root = tk.Tk() # suppress the default window
 root.withdraw() # suppress the default window
+import scipy.interpolate as interp
 from tkinter import filedialog # file UI
 #from tkinter import messagebox # file UI
 
@@ -33,7 +34,7 @@ maxVel = 25 # [ft/s] maximum robot velocity
 
 #------------------------------------------------------------------------------
 
-def textEntryWidget(title):
+def textEntryWidget(title,posx_init,posy_init):
     """ Creates a blocking text entry widget
     Args:
         title: name of the entry box
@@ -43,29 +44,50 @@ def textEntryWidget(title):
         None
     """
     
-    global user_input
+    global posx, posy, velocity, orientation
     def widgetClose(*args):
-        global user_input
-        user_input = entryField.get()
+        global posx, posy, velocity, orientation
+        posx = entryFieldPosX.get()
+        posy = entryFieldPosY.get()
+        velocity = entryFieldVelocity.get()
+        orientation = entryFieldOrientation.get()
         textwindow.quit()
         textwindow.destroy()
     
     # Open the text entry widget
     textwindow = tk.Tk()
     textwindow.title(title)
-    textwindow.geometry('400x50')
+    textwindow.geometry('400x400')
     textwindow.configure(background='#%02x%02x%02x' % ((245,245,245)))
     
     # Configure to handle use of the Windows close button
     textwindow.protocol('WM_DELETE_WINDOW',widgetClose)
     
     # Create, pack, and display the widget
-    entryField = tk.Entry(textwindow,fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),state=tk.NORMAL,width=30)
-    entryField.pack()
-    entryField.focus_set()
+    entryFieldPosX = tk.Entry(textwindow,fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),state=tk.NORMAL,width=30)
+    entryFieldPosY = tk.Entry(textwindow,fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),state=tk.NORMAL,width=30)
+    entryFieldVelocity = tk.Entry(textwindow,fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),state=tk.NORMAL,width=30)
+    entryFieldOrientation = tk.Entry(textwindow,fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),state=tk.NORMAL,width=30)
+    labelFieldPosX = tk.Label(textwindow,text='X Position (in)',fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),height=1,width=30,anchor='w')
+    labelFieldPosY = tk.Label(textwindow,text='Y Position (in)',fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),height=1,width=30,anchor='w')  
+    labelFieldVelocity = tk.Label(textwindow,text='Velocity (ft/s)',fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),height=1,width=30,anchor='w')
+    labelFieldOrientation = tk.Label(textwindow,text='Orientation (deg)',fg='#%02x%02x%02x' % ((0,0,0)),bg='#%02x%02x%02x' % ((255,255,255)),font=('Arial',16),height=1,width=30,anchor='w')         
+    labelFieldPosX.pack()                              
+    entryFieldPosX.pack()
+    labelFieldPosY.pack()
+    entryFieldPosY.pack()
+    labelFieldVelocity.pack()
+    entryFieldVelocity.pack()
+    labelFieldOrientation.pack()
+    entryFieldOrientation.pack()
+    entryFieldPosX.focus_set()
+    posx_init = posx_init/scale_pi;
+    posy_init = (fieldy_pixels-posy_init)/scale_pi;
+    entryFieldPosX.insert(0,'%0.2f' %(posx_init))
+    entryFieldPosY.insert(0,'%0.2f' %(posy_init))
     textwindow.mainloop()
     
-    return user_input
+    return posx, posy, velocity, orientation
 
 def safePtSelection(h_fig,ax,mkrType,mkrSize):
     """" Allows the user to select points on a figure window without closing the window failing silently
@@ -91,28 +113,38 @@ def safePtSelection(h_fig,ax,mkrType,mkrSize):
     ptxs = [] # initialize
     ptys = [] # initialize
     vels = [] # initialize
+    oris = [] # initialize
     while(flag_abort==False):
         pt = ginput(1,show_clicks=True,timeout=0.25) # check for click
         if(len(pt)!=0): 
             
             # Ask for the velocity
-            vel = textEntryWidget('Target Velocity (ft/s)')
+            [posx,posy,vel,ori] = textEntryWidget('Target Velocity (ft/s)',pt[0][0],pt[0][1])
+            try: 
+                posx = np.float(posx)
+                posx = posx*scale_pi
+            except: posx = pt[0][0]
+            try: 
+                posy = np.float(posy)
+                posy = fieldy_pixels-posy*scale_pi
+            except: posy = pt[0][1]
             try: vel = np.float(vel)
             except: vel = 0.0
+            try: ori = np.float(ori)
+            except: ori = 'ahead'
             
             # Append the user inputs
-            ptxs.append(pt[0][0])
-            ptys.append(pt[0][1])
+            ptxs.append(posx)
+            ptys.append(posy)
             vels.append(vel)
+            oris.append(ori)
             
             # Determine the color for the waypoint dot
             ptColor = plt.cm.plasma(vel/maxVel)
             ptColor = np.array([ptColor[0],ptColor[1],ptColor[2]])
-            ax.scatter(pt[0][0],pt[0][1],color=ptColor,marker=mkrType,s=mkrSize)
+            ax.scatter(posx,posy,color=ptColor,marker=mkrType,s=mkrSize)
             
-    # Reshape list into 2D numpy array
-    
-    return ptxs, ptys, vels
+    return ptxs, ptys, vels, oris
 
 #------------------------------------------------------------------------------
 
@@ -142,9 +174,11 @@ ax.set_ylim((I.shape[0],0))
 # Generate tick marks and grid lines
 numyticks = 5 # number of ticks to show along the x-axis
 numxticks = 10 # number of ticks to show along the y-axis
-scale = I.shape[0]/fieldy # [pix/ft] recover the scaling between the image and real life
-xticks_num = (np.round(np.linspace(0,I.shape[1],numxticks+1)/scale)).astype(int)
-yticks_num = (np.round(np.linspace(0,I.shape[0],numyticks+1)/scale)).astype(int)
+scale_pf = I.shape[0]/fieldy # [pix/ft] recover the scaling between the image and real life
+scale_pi = scale_pf/12 # [pix/inches]
+fieldy_pixels = I.shape[0] # [pixels] width of the field in pixels
+xticks_num = (np.round(np.linspace(0,I.shape[1],numxticks+1)/scale_pf)).astype(int)
+yticks_num = (np.round(np.linspace(0,I.shape[0],numyticks+1)/scale_pf)).astype(int)
 xticks_str = []
 yticks_str = []
 for i in range(0,len(xticks_num),1):
@@ -154,8 +188,8 @@ for i in range(0,len(yticks_num),1):
 yticks_str = np.flip(yticks_str,0)
 ax.set_xticklabels(xticks_str)
 ax.set_yticklabels(yticks_str)
-xstep = (scale*fieldx/(numxticks))
-ystep = (scale*fieldy/(numyticks))
+xstep = (scale_pf*fieldx/(numxticks))
+ystep = (scale_pf*fieldy/(numyticks))
 xticklocs = np.zeros((numxticks+1),float)
 yticklocs = np.zeros((numyticks+1),float)
 xticklocs[0:numxticks] = np.arange(0,I.shape[1]-0.1,xstep)
@@ -176,15 +210,29 @@ plt.title('Left-Click to Select Waypoints',fontsize=24)
 plt.draw()
 
 # Have the user select the waypoints
-[ptxs,ptys,vels] = safePtSelection(h_fig,ax,'o',100)
+[ptxs,ptys,vels,oris] = safePtSelection(h_fig,ax,'o',100)
+
+# Convert the selected point coordinates to real units
+ptxs = np.array(ptxs)/scale_pi # [in]
+ptys = (fieldy_pixels-np.array(ptys))/scale_pi # [in]
+vels = 12.0*np.array(vels) # [in/s]
+
+# Show waypoints
 print(ptxs)
 print(ptys)
 print(vels)
+print(oris)
 
-# Convert the selected point coordinates to real units
-#*** remember to flip y value
-
+# Cubic spline interpolation
+interp.CubicSpline()
 
 #*** also need to specify robot orientation along with velocity
+
+
+
+
+
+
+
 
 
