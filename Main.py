a688f24f88fe9c3204bc35d8cdc2ce9f61f2de29
lrscime <lrscime.alpha@gmail.com>
2019-10-05 00:11:24 -0400

    Adds the point interpolation and smoothing algorithms.

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-# Date: 2019-09-30
+# Date: 2019-10-4
 # Author: Secret City Wildbots
 # Description: allows the user to load an image of the field and generate a 
 #              planned path for the robot to follow
@@ -12,7 +12,6 @@ from pylab import ginput # get user mouse inputs
 import tkinter as tk # file UI
 root = tk.Tk() # suppress the default window
 root.withdraw() # suppress the default window
-import scipy.interpolate as interp
 from tkinter import filedialog # file UI
 #from tkinter import messagebox # file UI
 
@@ -30,7 +29,7 @@ plt.rcParams.update({'font.size': 24}) # change the default font size for plots
 fieldx = 54.0 # [feet] field length
 fieldy = 27.0 # [feet] field width
 res = 100 # controls the resolution of the displayed image
-maxVel = 25 # [ft/s] maximum robot velocity
+maxVel = 100 # [ft/s] maximum robot velocity
 
 #------------------------------------------------------------------------------
 
@@ -210,23 +209,286 @@ plt.title('Left-Click to Select Waypoints',fontsize=24)
 plt.draw()
 
 # Have the user select the waypoints
-[ptxs,ptys,vels,oris] = safePtSelection(h_fig,ax,'o',100)
+[ptxs_way,ptys_way,vels_way,oris_way] = safePtSelection(h_fig,ax,'o',100)
 
 # Convert the selected point coordinates to real units
-ptxs = np.array(ptxs)/scale_pi # [in]
-ptys = (fieldy_pixels-np.array(ptys))/scale_pi # [in]
-vels = 12.0*np.array(vels) # [in/s]
+ptxs_way = np.array(ptxs_way)/scale_pi # [in]
+ptys_way = (fieldy_pixels-np.array(ptys_way))/scale_pi # [in]
+vels_way = 12.0*np.array(vels_way) # [in/s]
 
 # Show waypoints
-print(ptxs)
-print(ptys)
-print(vels)
-print(oris)
+print(ptxs_way)
+print(ptys_way)
+print(vels_way)
+print(oris_way)
 
-# Cubic spline interpolation
-interp.CubicSpline()
+#ptxs_way = np.array([ 64.11,  96.94, 148.74, 180.65, 196.83, 195.  , 195.  , 195.  ,
+#       214.87, 243.54, 258.33, 264.81])
+#ptys_way = np.array([190.  , 190.  , 190.  , 204.4 , 229.37, 247.86, 264.51, 284.86,
+#       295.96, 284.4 , 256.19, 226.13])
+#vels_way = 12*np.array([0., 2., 10., 10., 10., 9., 8., 7., 6., 10., 5., 0.])
 
-#*** also need to specify robot orientation along with velocity
+#------------------------------------------------------------------------------
+
+# Insert dense points along a piece-wise linear interpolation
+delta_s = 1 # [inches] step length along the path
+vels_dense = [] # [in/s] dense v points along the original path
+ptxs_segs = [] # [inches] the dense x points listed per segment
+ptys_segs = [] # [inches] the dense y points listed per segment
+for i in range(0,len(ptxs_way)-1,1):
+    
+    # Initialize
+    ptxs_seg = []
+    ptys_seg = []
+    
+    # Calculate the length of the path segment
+    max_s = np.sqrt(((ptys_way[i+1]-ptys_way[i])**2)+((ptxs_way[i+1]-ptxs_way[i])**2)) 
+    
+    # Calculate the steps for the current path segment 
+    gamma = np.arctan2((ptys_way[i+1]-ptys_way[i]),(ptxs_way[i+1]-ptxs_way[i]))
+    delta_x = delta_s*np.cos(gamma)
+    delta_y = delta_s*np.sin(gamma)
+    delta_v = (vels_way[i+1]-vels_way[i])/max_s
+    
+    # Include the current waypoint
+    ptxs_seg.append(ptxs_way[i]) 
+    ptys_seg.append(ptys_way[i]) 
+    vels_dense.append(vels_way[i])
+    
+    # Step the inserted points along the path
+    total_s = 0 # tracks total travel along the current path segment
+    
+    while((total_s+delta_s)<max_s):
+        
+        ptxs_seg.append(ptxs_seg[-1]+delta_x)
+        ptys_seg.append(ptys_seg[-1]+delta_y)
+        vels_dense.append(vels_dense[-1]+delta_v)
+        total_s += delta_s 
+        
+    # Also store the dense points segement-wise (needed for smoothing)
+    ptxs_segs.append(ptxs_seg)
+    ptys_segs.append(ptys_seg)
+        
+#------------------------------------------------------------------------------
+    
+# Smooth the path
+
+r = 24 # [inches] minimum turn radius *** could scale as f(v)
+
+ptxs_smooth = []
+ptys_smooth = []
+vels_smooth = []
+aNum_start = 0 # start point for the current segment
+
+for segNum in range(0,(len(ptxs_segs)-1),1):
+    
+    # Initialize
+    flag_corner = False # False if not reached a corner yet
+    
+    # Calculate (Q-I) gamma 
+    gamma = np.arctan2(np.abs((ptys_way[segNum+1]-ptys_way[segNum])),np.abs((ptxs_way[segNum+1]-ptxs_way[segNum])))
+    
+    # Calculate the perpendicular offsets from the current segment
+    delta_x = r*np.sin(gamma)
+    delta_y = r*np.cos(gamma)
+    
+    # Determine the slope of the current line segment
+    try:
+        m = (ptys_way[segNum+1]-ptys_way[segNum])/(ptxs_way[segNum+1]-ptxs_way[segNum])
+        if(m>=0.0): seg_slope = 'positive'
+        else: seg_slope = 'negative'
+    except: seg_slope = 'positive' # catch vertical slope
+    
+    for aNum in range(aNum_start,len(ptxs_segs[segNum]),1):
+        
+        # Calculate the centers (h,k) of the correct tangent circles
+        xa = ptxs_segs[segNum][aNum] # current point in the current segment
+        ya = ptys_segs[segNum][aNum] # current point in the current segment
+        if(seg_slope=='positive'):
+            h1 = xa + delta_x
+            k1 = ya - delta_y
+            h2 = xa - delta_x
+            k2 = ya + delta_y
+        elif(seg_slope=='negative'):
+            h1 = xa + delta_x
+            k1 = ya + delta_y
+            h2 = xa - delta_x
+            k2 = ya - delta_y
+            
+        for bNum in range(0,len(ptxs_segs[segNum+1]),1):
+            
+            # Calculate distance from a point in the next segment to the circle centers
+            xb = ptxs_segs[segNum+1][bNum]
+            yb = ptys_segs[segNum+1][bNum]
+            d1 = np.sqrt(((h1-xb)**2)+((k1-yb)**2))
+            d2 = np.sqrt(((h2-xb)**2)+((k2-yb)**2))
+            
+            # Check if the point in the next segment is inside of one of the tangent circles
+            if((d1<=r)|(d2<=r)):
+                
+                # Determine the number of points which must be smoothed
+                numPtsSmooth = (len(ptxs_segs[segNum]) - aNum) + (bNum+1)
+                
+                # Check which candidate circle is the correct one
+                if(d1<=r):
+                    h = h1
+                    k = k1
+                else:
+                    h = h2
+                    k = k2
+                    
+                # Calculate the angle intervals
+                phia = np.arctan2((ya-k),(xa-h))
+                if(phia<0): phia += 2.0*np.pi
+                phib = np.arctan2((yb-k),(xb-h))
+                if(phib<0): phib += 2.0*np.pi
+                
+                # Choose the interior angle
+                delta_phi = np.abs(phia-phib)
+                phi_step = delta_phi/(numPtsSmooth-1)
+                phis = []
+                if(phia>phib):
+                    # Initerior is clockwise
+                    phi_moving = phia
+                    for q in range(0,numPtsSmooth,1):
+                        phis.append(phi_moving)
+                        phi_moving -= phi_step
+                else:
+                    # Interior is anti-clockwise
+                    phi_moving = phia
+                    for q in range(0,numPtsSmooth,1):
+                        phis.append(phi_moving)
+                        phi_moving += phi_step
+                
+                
+                #***
+                if(phia>phib): fakev = 1000
+                else: fakev = 500
+                #***
+                
+                
+                # Smooth points in current segment
+                phis_index = 0
+                for i in range(aNum,len(ptxs_segs[segNum]),1):
+                    xa_smooth = h + r*np.cos(phis[phis_index])
+                    ya_smooth = k + r*np.sin(phis[phis_index])
+                    ptxs_smooth.append(xa_smooth)
+                    ptys_smooth.append(ya_smooth)
+                    vels_smooth.append(fakev) #***
+                    phis_index += 1
+                
+                # Smooth points in next segment
+                for i in range(0,(bNum+1),1):
+                    xa_smooth = h + r*np.cos(phis[phis_index])
+                    ya_smooth = k + r*np.sin(phis[phis_index])
+                    ptxs_smooth.append(xa_smooth)
+                    ptys_smooth.append(ya_smooth)
+                    vels_smooth.append(fakev) #***
+                    phis_index += 1
+                
+                # Set corner flag
+                flag_corner = True
+                break
+                
+        # Add the current point in the current segment to the smoothed path if it's chill
+        if(flag_corner==False):
+            ptxs_smooth.append(xa)
+            ptys_smooth.append(ya)
+            vels_smooth.append(0.0)
+            aNum_start = 0 # start the next segment at the beginning
+        else: 
+            aNum_start = bNum+1 # start the next segment after the radius
+            break
+
+# Include the last line segment in the smoothed path
+for i in range(aNum_start,len(ptxs_segs[-1]),1):
+    ptxs_smooth.append(ptxs_segs[-1][i])
+    ptys_smooth.append(ptys_segs[-1][i])
+    vels_smooth.append(0.0)
+ptxs_smooth.append(ptxs_way[-1])
+ptys_smooth.append(ptys_way[-1])
+vels_smooth.append(0.0)
+
+
+
+#------------------------------------------------------------------------------
+
+# Calculate the distance, time, and curvature along the path
+#***
+
+#------------------------------------------------------------------------------
+
+#*** should also plot original waypoints
+
+# Create the figure
+h_fig = plt.figure('Wildbot''s Autonomous Path Planner',[15,15],facecolor='w')
+ax = plt.gca() # get current axis
+ax.axis('image') # set axis type
+ax.set_xlim((0,I.shape[1]))        
+ax.set_ylim((I.shape[0],0))
+
+# Generate tick marks and grid lines
+numyticks = 5 # number of ticks to show along the x-axis
+numxticks = 10 # number of ticks to show along the y-axis
+scale_pf = I.shape[0]/fieldy # [pix/ft] recover the scaling between the image and real life
+scale_pi = scale_pf/12 # [pix/inches]
+fieldy_pixels = I.shape[0] # [pixels] width of the field in pixels
+xticks_num = (np.round(np.linspace(0,I.shape[1],numxticks+1)/scale_pf)).astype(int)
+yticks_num = (np.round(np.linspace(0,I.shape[0],numyticks+1)/scale_pf)).astype(int)
+xticks_str = []
+yticks_str = []
+for i in range(0,len(xticks_num),1):
+    xticks_str.append(str(xticks_num[i]))
+for i in range(0,len(yticks_num),1):
+    yticks_str.append(str(yticks_num[i])) 
+yticks_str = np.flip(yticks_str,0)
+ax.set_xticklabels(xticks_str)
+ax.set_yticklabels(yticks_str)
+xstep = (scale_pf*fieldx/(numxticks))
+ystep = (scale_pf*fieldy/(numyticks))
+xticklocs = np.zeros((numxticks+1),float)
+yticklocs = np.zeros((numyticks+1),float)
+xticklocs[0:numxticks] = np.arange(0,I.shape[1]-0.1,xstep)
+yticklocs[0:numyticks] = np.arange(0,I.shape[0]-0.1,ystep)
+xticklocs[numxticks] = I.shape[1]
+yticklocs[numyticks] = I.shape[0]
+ax.set_xticks(xticklocs,minor=False)
+ax.set_yticks(yticklocs,minor=False)
+plt.grid(b=None,which='major',axis='both',color=np.array([0.8,0.8,0.8]))
+
+# Label the axes    
+plt.xlabel('Length (ft)')
+plt.ylabel('Width (ft)')
+        
+# Display the image        
+h_im = plt.imshow(I_color,extent=[0,I.shape[1],I.shape[0],0])
+plt.draw()
+
+# Display the path
+for i in range(0,len(ptxs_smooth),1):
+    ptColor = plt.cm.plasma(vels_smooth[i]/(12*maxVel))
+    ptColor = np.array([ptColor[0],ptColor[1],ptColor[2]])
+    ax.scatter(ptxs_smooth[i]*scale_pi,fieldy_pixels-(ptys_smooth[i]*scale_pi),color=ptColor,marker='.',s=50)
+
+#------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
