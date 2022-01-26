de01efd8065db986efa32b820c3708612d4ec239
lrscime <lrscime.alpha@gmail.com>
2022-01-25 22:01:19 -0500

    -switching back to dev

--- code/VersioningControl.py
+++ code/VersioningControl.py
@@ -186,6 +186,9 @@ created by FIRST Robotics Team 4265.
 Release Notes
 -------------------------------------------------------------------------------------
 
+v2.2.4
+***
+
 v2.2.2
 > Updates the default values for the 2022 Rapid React FRC Game.
 
